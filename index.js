340ce891549f6e911f354b9c1bb6f4ba4ca6c254
venom-napsta <napstakid@gmail.com>
2022-10-08 15:11:32 +0200

    2.0

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/hello", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+module.exports=app;
